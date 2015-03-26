20c2b36adf13ec156ae1b530bb44790f3a7d3747
Vitor Margis <vitor.margis@gmail.com>
2015-03-26 14:10:40 -0300

    Change gulp-connet pot to :8181

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
   connect = require('gulp-connect');
 
 gulp.task('webserver', function() {
-  connect.server();
+  connect.server({
+    port: 8181
+  });
 });
 
 gulp.task('default', ['webserver']);
