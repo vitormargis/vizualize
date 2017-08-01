f59b322d44342462f9be54f052cf0e13566ba42b
Vitor Margis <vitor.margis@gmail.com>
2017-08-01 01:06:05 -0300

    Remove comments

--- app/routes.js
+++ app/routes.js
@@ -4,14 +4,6 @@
   angular.module('vizualize').config(function($stateProvider, $urlRouterProvider, $locationProvider ) {
     $urlRouterProvider.otherwise('');
     $locationProvider.html5Mode(true);
-    // $urlRouterProvider.rule(function ($injector, $location) {
-    //   var path = $location.url();
-    //
-    //   if (path[path.length - 1] === '/' || path.indexOf('/?') > -1) return;
-    //   if (path.indexOf('?') > -1) return path.replace('?', '/?');
-    //
-    //   return path + '/';
-    // });
 
     $stateProvider
       .state('home', {
